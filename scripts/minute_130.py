6c67781a94bec1321a8dd957f4fe9908f9621ef6
Layanah-20250279 <lay20250279@std.psut.edu.jo>
2026-01-09 11:53:19 +0300

    updated minute_130.py script
    Changed minute 130's script to indicate the near end of the match better.

--- scripts/minute_130.py
+++ scripts/minute_130.py
@@ -1,1 +1,4 @@
-print('Players are playing, fans are praying.')
+print('Final chance for the players to turn events.')
+# Layanah Hamadneh, 20250279
+# I changed the term 'Players are playing, fans are praying' to 'Final chance for the players to turn events.'
+
